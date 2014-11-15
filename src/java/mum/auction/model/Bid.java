593af64dbcbe2ca893de49bc5d99105bcbb7641d
Fetiya <Fetiya@10.10.52.190>
2014-11-15 16:39:30 -0600

    Bid class added

---
+++ src/java/mum/auction/model/Bid.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mum.auction.model;
+
+/**
+ *
+ * @author Fetiya
+ */
+public class Bid {
+    
+    private int Id;
+    private Auction auction;
+    private User user;
+    private int offeredPrice;
+
+    public Bid() {
+    }
+
+    public Bid(Auction auction, User user, int offeredPrice) {
+        this.auction = auction;
+        this.user = user;
+        this.offeredPrice = offeredPrice;
+    }
+
+    public Auction getAuction() {
+        return auction;
+    }
+
+    public void setAuction(Auction auction) {
+        this.auction = auction;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getOfferedPrice() {
+        return offeredPrice;
+    }
+
+    public void setOfferedPrice(int offeredPrice) {
+        this.offeredPrice = offeredPrice;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
